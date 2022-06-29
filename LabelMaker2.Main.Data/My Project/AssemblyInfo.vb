bc63198602f02b45d5474f374ae5f0e41a79e1fe
Carl Neumann <neumann@geoconcepts.com>
2022-06-29 15:08:54 -0400

    Update to Net 4.8

--- LabelMaker2.Main.Data/My Project/AssemblyInfo.vb
+++ LabelMaker2.Main.Data/My Project/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("8fe69a7c-fcd9-47e0-b783-7d4239e046f0")> 
+<Assembly: Guid("8fe69a7c-fcd9-47e0-b783-7d4239e046f0")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.0.0.8")>
+<Assembly: AssemblyFileVersion("1.0.0.8")>
